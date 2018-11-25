809bc95f0f11f39b3a48a167cc345e5dcab03899
Lucy <lucynn11@gmail.com>
2018-11-25 20:00:29 +0400

    getting head and tail to work

--- commands.js
+++ commands.js
@@ -6,26 +6,37 @@ function done(output){
     process.stdout.write('\nprompt > ');
 }
 
-//where we will store our commands
+//where the commands will be stored
 function evaluateCmd(userInput){
     
-    //parses the user input to understand whic command was typd
+    //parses the user input to understand which command was typed
     const userInputArray = userInput.split(" ");
     const command = userInputArray[0];
 
 
     switch (command){
         case "echo":
-            //we will add the functionality of echo next within the object commandLibrary
+            //adding the functionality of echo next within the object commandLibrary
             commandLibrary.echo(userInputArray.slice(1).join(" "));
-            break;
+        break;
 
         case "cat":
             commandLibrary.cat(userInputArray.slice(1));
-            break;
+        break;
+
+        case "head":
+            commandLibrary.head(userInputArray.slice(4));
+        break;
+
+        case "tail":
+            commandLibrary.head(userInputArray.slice(4));
+        break;
+
+        default:
+            console.log("That is not a valid entry. Please try again.");
     }
 }
-//where we will store the logic for our commands
+//where the logic for our commands will be stored
 const commandLibrary = {
     //defining the echo command
     "echo": function(userInput) {
@@ -35,6 +46,24 @@ const commandLibrary = {
 
     //defining the cat command
     "cat": function(fullPath){
+        const fileName = fullPath[0];
+        fs.readFile(fileName, (err, data) =>{
+            if (err) throw err;
+            done(data);
+        });
+    },
+
+    //defining the head command
+      "head": function(fullPath){
+        const fileName = fullPath[2];
+        fs.readFile(fileName, (err, data) =>{
+            if (err) throw err;
+            done(data);
+        });
+    },
+
+    //defining the tail command
+      "tail": function(fullPath){
         const fileName = fullPath[0];
         fs.readFile(fileName, (err, data) =>{
             if (err) throw err;
